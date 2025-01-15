fc0d170aaac848f941ee5c410be2e7a3642b8362
Thamili | தமிழி <121654749+dondanu@users.noreply.github.com>
2025-01-15 10:40:32 +0530

    final of all

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Default Route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
 // Error handling
